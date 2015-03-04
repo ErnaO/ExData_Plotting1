c2be6b21efee957b3048e4112af3615ccf15ea33
ErnaO <ernaoudman@hotmail.com>
2015-03-04 20:45:20 +0100

    And plot 3

--- plot2.R
+++ plot2.R
@@ -8,15 +8,18 @@ Sys.setlocale("LC_TIME", "English")
 hpc <- read.table("./household_power_consumption.txt", sep=";", header=T, na.strings="?")
 hpc$Date <- as.Date(hpc$Date,format="%d/%m/%Y")
 
-
 # Getting the subset we need: 2007-02-01 and 2007-02-02
 idx <- which(hpc$Date == as.Date("2007-02-01") | hpc$Date == as.Date("2007-02-02"))
 sub_hpc <-hpc[idx,]
 
 # Combine date and time
 td <- paste(sub_hpc$Date,sub_hpc$Time)
-td1 <- strptime(td,format="%Y-%m-%d %H:%M:%S")
+sub_hpc$DateTime <- strptime(td,format="%Y-%m-%d %H:%M:%S")
 
 # Making the plot
-plot(td1,sub_hpc$Global_active_power,type='l',
+plot(sub_hpc$DateTime,sub_hpc$Global_active_power,type='l',
      ylab="Global Active Power (kilowatts)",xlab='')
+
+# Writing the plot to PNG
+dev.copy(png, "./plot2.png",width=480,height=480)
+dev.off()
