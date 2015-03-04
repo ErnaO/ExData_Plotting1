2b915aa7f3fed6c859bc88a03d7ea3880070413d
ErnaO <ernaoudman@hotmail.com>
2015-03-04 19:42:33 +0100

    The first plot

---
+++ plot1.R
@@ -0,0 +1,22 @@
+## Course Project 1
+## Plot 1
+
+library(lubridate)
+
+# Load the data
+hpc <- read.table("./household_power_consumption.txt", sep=";", header=T, na.strings="?")
+hpc$Date <- as.Date(hpc$Date,format="%d/%m/%Y")
+
+# Getting the subset we need: 2007-02-01 and 2007-02-02
+day1 <- as.Date("2007-02-01")
+idx <- which(hpc$Date == as.Date("2007-02-01") | hpc$Date == as.Date("2007-02-02"))
+sub_hpc <-hpc[idx,]
+
+# Making the plot
+hist(sub_hpc$Global_active_power,col="red",main="Global Active Power",
+     xlab="Global Active Powerr (kilowatts)", ylab="Frequency")
+
+# Writing the plot to PNG
+dev.copy(png, "./plot1.png",width=480,height=480)
+dev.off()
+
